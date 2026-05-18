26e645e2992c46c3bfbed25b9c5269c9166f5593
Dori Moore <escrow.ringer9c@icloud.com>
2026-05-18 18:55:31 -0400

    Recreated in Vue.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template> 
+  <div>
+    <h1>A Garden Journal</h1>
+
+    <input placeholder="Plant name" v-model="name" />
+    <input placeholder="Notes" v-model="notes" />
+    <button @click="handleAddPlant">Add Plant</button>
+
+    <div v-for="plant in plants" :key="plant.id">
+      <h2>{{ plant.name }}</h2>
+      <p>{{ plant.notes }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const plants = ref<{ id: number; name: string; notes: string }[]>([])
+const name = ref('')
+const notes = ref('')
+
+function handleAddPlant() {
+  if (name.value === '')  return
+  plants.value.push({ id: Date.now(), name: name.value, notes: notes.value})
+  name.value = ''
+  notes.value = ''
+}
+
+</script>
+
+<style>
+</style>
