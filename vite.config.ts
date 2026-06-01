d42a9dfa6a9108f655e8da873bc8f3abb3670f84
Dori Moore <escrow.ringer9c@icloud.com>
2026-06-01 14:56:55 -0400

    Merging conflicts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
-<<<<<<< HEAD
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-=======
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> origin/main
 })
