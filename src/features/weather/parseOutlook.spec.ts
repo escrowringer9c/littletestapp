b540eb69175e41faebf39dc1470913548db13dde
Dori Moore <escrow.ringer9c@icloud.com>
2026-06-29 11:52:08 -0400

    One more test that proves JS handles unexpected sorting well.

--- src/features/weather/parseOutlook.spec.ts
+++ src/features/weather/parseOutlook.spec.ts
@@ -15,4 +15,38 @@ describe('parseOutlook', () => {
         expect(result.highestRisk.label).toBe('NONE')
         expect(result.highestRisk.label2).toBe('No Severe Weather')
     })
+
+
+
+    it('ignores features with missing DN when finding highest risk', () => {
+        const malformedOutlook = {
+            features: [
+                {
+                    properties: {
+                        DN: 3,
+                        LABEL: 'MRGL',
+                        LABEL2: 'Marginal Risk',
+                        fill: '#66A366',
+                        stroke: '#005500',
+                        VALID_ISO: '2026-06-19T20:00:00+00:00',
+                        EXPIRE_ISO: '2026-06-20T12:00:00+00:00',
+                        FORECASTER: 'Lyons',
+                    }
+                },
+                {
+                    properties: {
+                        LABEL: 'TSTM',
+                        LABEL2: 'General Thunderstorms Risk',
+                        fill: '#C1E9C1',
+                        stroke: '#55BB55',
+                        VALID_ISO: '2026-06-19T20:00:00+00:00',
+                        EXPIRE_ISO: '2026-06-20T12:00:00+00:00',
+                        FORECASTER: 'Lyons',
+                    }
+                }
+            ]
+        }
+        const result = parseOutlook(malformedOutlook)
+        expect(result.highestRisk.label).toBe('MRGL')
+    })
 })
